7ad3590a2e5d068e2d6b59c04678e54bbfbfc290
de3panshu <sahu.deeepanshu.united@gmail.com>
2026-02-09 13:09:58 +0530

    setup required libraries

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-success'>Added the required libraries</div>
+    </>
+  )
+}
+
+export default App
